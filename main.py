ba69482c7790dce1a776f6661d0b6587f56cb117
hoanganh2006 <32439731+hoanganh2006@users.noreply.github.com>
2018-02-06 15:01:28 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,81 +1,104 @@
-import os
+import os 
 import csv
-election_1 = os.path.join("election_data_1.csv")
-with open (election_1, 'r', newline = "") as csvfile: 
-	election_data_1 = csv.reader(csvfile, delimiter = ",")
-	next(election_data_1)
+import string
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+        }
+employee_1 = os.path.join("employee_data1.csv")
+with open (employee_1, 'r', newline = "") as csvfile: 
+	employee_1 = csv.reader(csvfile, delimiter = ",")
+	next(employee_1)
+	employee_1 = list(employee_1)
 	text_file_1 = open("Output_1.txt", "w")
-	election_data_1 = list(election_data_1)
-	total_votes = len(election_data_1)
-	candidate = {}
-	number_of_election = 0
-	for row in election_data_1: 
-		if row[2] not in candidate.keys():
-			number_of_election = 0
-			candidate.update({row[2]: number_of_election})
-		if row[2] in candidate.keys(): 
-			candidate[row[2]] += 1
-	print("Election Results")
-	text_file_1.write("Election Results" + '\n')
-	print("-----------------------------------")
-	text_file_1.write("-----------------------------------" + '\n')
-	print("Total Votes: " + str(total_votes))
-	text_file_1.write("Total Votes: " + str(total_votes) + '\n')
-	print("-----------------------------------")
-	text_file_1.write("-----------------------------------" + '\n')
-	for keys, values in candidate.items():
-		print(keys + " : " + str((values/total_votes)*100) +"%" + " (" + str(values) + ")")
-		text_file_1.write(keys + " : " + str((values/total_votes)*100) +"%" + " (" + str(values) + ")" + '\n')
-	print("-----------------------------------")
-	text_file_1.write("-----------------------------------" + '\n')
-	winner = max(candidate.values())
-	for key, value in candidate.items():
-		if winner == value: 
-			print("Winner: " + key)
-			text_file_1.write("Winner: " + key + '\n')
-	print("-----------------------------------")
-	text_file_1.write("-----------------------------------" + '\n')
-text_file_1.close()
+	text_file_1.write("Emp ID,First Name,Last Name,DOB,SSN,State" + '\n')
+	print("Emp ID,First Name,Last Name,DOB,SSN,State")
+	for employee in employee_1: 
+		employee[1] = employee[1].replace(" ", ",")
+		employee[2] = ("/".join([employee[2][5:7], employee[2][8:10], employee[2][0:4]]))
+		employee[3] = employee[3].replace(employee[3][0:6], "***-**")
+		for key, value in states.items():
+			if employee[4] == value:
+				employee[4] = key
+		print(employee[0] + "," + employee[1] + "," + employee[2] + "," + employee[3] + "," + employee[4])
+		text_file_1.write(employee[0] + "," + employee[1] + "," + employee[2] + "," + employee[3] + "," + employee[4] + '\n')
+	text_file_1.close()
 csvfile.close()
 
 #Open the second file and do the same things
 
-election_2 = os.path.join("election_data_2.csv")
-with open (election_2, 'r', newline = "") as csvfile:
-	election_data_2 = csv.reader(csvfile, delimiter = ",")
-	next(election_data_2)
+employee_2 = os.path.join("employee_data2.csv")
+with open (employee_2, 'r', newline = "") as csv_file: 
+	employee_2 = csv.reader(csv_file, delimiter = ",")
+	next(employee_2)
+	employee_2 = list(employee_2)
 	text_file_2 = open("Output_2.txt", "w")
-	election_data_2 = list(election_data_2)
-	total_votes = len(election_data_2)
-	candidate = {}
-	number_of_election = 0
-	for row in election_data_2: 
-		if row[2] not in candidate.keys():
-			number_of_election = 0
-			candidate.update({row[2]: number_of_election})
-		if row[2] in candidate.keys(): 
-			candidate[row[2]] += 1
-	print("Election Results")
-	text_file_2.write("Election Results" + '\n')
-	print("-----------------------------------")
-	text_file_2.write("-----------------------------------" + '\n')
-	print("Total Votes: " + str(total_votes))
-	text_file_2.write("Total Votes: " + str(total_votes) + '\n')
-	print("-----------------------------------")
-	text_file_2.write("-----------------------------------" + '\n')
-	for keys, values in candidate.items():
-		print(keys + " : " + str((values/total_votes)*100) +"%" + " (" + str(values) + ")")
-		text_file_2.write(keys + " : " + str((values/total_votes)*100) +"%" + " (" + str(values) + ")" + '\n')
-	print("-----------------------------------")
-	text_file_2.write("-----------------------------------" + '\n')
-	winner = max(candidate.values())
-	for key, value in candidate.items():
-		if winner == value: 
-			print("Winner: " + key)
-			text_file_2.write("Winner: " + key + '\n')
-	print("-----------------------------------")
-	text_file_2.write("-----------------------------------" + '\n')
-text_file_2.close()
-csvfile.close()
-
+	text_file_2.write("Emp ID,First Name,Last Name,DOB,SSN,State" + '\n')
+	print("Emp ID,First Name,Last Name,DOB,SSN,State")
+	for employee in employee_2: 
+		employee[1] = employee[1].replace(" ", ",")
+		employee[2] = ("/".join([employee[2][5:7], employee[2][8:10], employee[2][0:4]]))
+		employee[3] = employee[3].replace(employee[3][0:6], "***-**")
+		for key, value in states.items():
+			if employee[4] == value:
+				employee[4] = key
+		print(employee[0] + "," + employee[1] + "," + employee[2] + "," + employee[3] + "," + employee[4])
+		text_file_2.write(employee[0] + "," + employee[1] + "," + employee[2] + "," + employee[3] + "," + employee[4] + '\n')
+	text_file_2.close()
+csv_file.close()
 
